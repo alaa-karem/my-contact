57bd02d25b408a351108acf07865e627fd61a3f9
alaa <alaakarem182002@gmail.com>
2024-12-29 18:09:33 +0200

    Adding buttom sheet

--- lib/widgets/social_media_icons.dart
+++ lib/widgets/social_media_icons.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_contacts/start.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class SocialMediaIcon extends StatelessWidget {
@@ -22,10 +21,17 @@ class SocialMediaIcon extends StatelessWidget {
           radius: 40,
         ),
         onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => StartSocialMedia(socialMediaLink: socialMediaLink,)),
-          );
+          showModalBottomSheet(context: context, 
+          builder: (context){
+            return ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.deepOrange)),
+            onPressed: () {
+              launchUrl(Uri.parse(socialMediaLink),
+                  mode: LaunchMode.externalApplication);
+            },
+            child: Text("Start $socialMedia"));
+          });
         },
       ),
     );
